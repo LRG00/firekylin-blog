b53e5e8e90d9b95230f5c2a7d09d511e0319010e
leeruigan <leeruigan@gmail.com>
2019-05-28 15:19:56 +0800

    init

---
+++ src/common/config/db.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+exports.default = {
+    "type": "mysql",
+    "adapter": {
+        "mysql": {
+            "host": "202.182.122.247",
+            "port": "3306",
+            "database": "mysql",
+            "user": "root",
+            "password": "123456",
+            "prefix": "fk_",
+            "encoding": "utf8"
+        }
+    }
+}
+    
